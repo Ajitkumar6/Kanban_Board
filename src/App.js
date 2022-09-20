0460eeb6899af06a33a7af982aaa83fee26678ef
Ajitkumar6 <ajitcareerit@gmail.com>
2022-09-20 18:17:51 +0530

    kanban board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'h8k-components';
+import KanbanBoard from './components/kanban-board';
+const title = "React App";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+			<h8k-navbar header={title}></h8k-navbar>
+			<KanbanBoard />
+        </div>
+    );
 }
 
 export default App;

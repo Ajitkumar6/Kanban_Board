fdf2529e49da389ce6037fe2e1de3982c0c18ba5
Ajitkumar6 <ajitcareerit@gmail.com>
2022-09-20 19:24:11 +0530

    kanban

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import {applyPolyfills, defineCustomElements} from 'h8k-components/loader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
 applyPolyfills().then(() => {
     defineCustomElements(window);
 })
